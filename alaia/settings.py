e5599e6d12de282440b90c4515f60033059c9b59
Shana <shanackn@gmail.com>
2026-05-01 07:01:05 +0530

    fixed razorpay

--- alaia/settings.py
+++ alaia/settings.py
@@ -169,4 +169,8 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000',
 ]
+for host in ALLOWED_HOSTS:
+    if host not in ['*', '127.0.0.1', 'localhost']:
+        CSRF_TRUSTED_ORIGINS.append(f'http://{host}')
+        CSRF_TRUSTED_ORIGINS.append(f'https://{host}')
 
